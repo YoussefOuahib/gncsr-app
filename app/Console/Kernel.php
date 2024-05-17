188f530412cb7e8602e1f114a17dc6e07d80727e
Youssef Ouahib <youssefoh98@gmail.com>
2024-05-17 19:42:08 +0100

    fix timing of commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
-        $schedule->command('process:data')->everyTwentySeconds();
+        $schedule->command('process:data')->everyTenMinutes();
     }
 
     /**
